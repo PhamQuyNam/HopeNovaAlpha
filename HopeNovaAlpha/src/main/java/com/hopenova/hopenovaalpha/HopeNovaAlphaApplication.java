1c74bc30005b5d6ceac91a2fb558784429a3cd46
PhamQuyNam <phamquynam2004@gmail.com>
2026-06-15 22:39:31 +0700

    first commit

---
+++ HopeNovaAlpha/src/main/java/com/hopenova/hopenovaalpha/HopeNovaAlphaApplication.java
@@ -0,0 +1,14 @@
+package com.hopenova.hopenovaalpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopeNovaAlphaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HopeNovaAlphaApplication.class, args);
+    }
+
+
+}
---
+++ HopeNovaAlpha/src/test/java/com/hopenova/hopenovaalpha/HopeNovaAlphaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hopenova.hopenovaalpha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopeNovaAlphaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
